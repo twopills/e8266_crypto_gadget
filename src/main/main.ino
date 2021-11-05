e538e5a0b7d59c9ccb73b7295e9afa3005dd635a
twopill <ceravolo.elia@gmail.com>
2021-11-05 23:49:47 +0100

    fix return data

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,8 +7,7 @@
 #define BUTTON_PIN 0
 EasyButton button(BUTTON_PIN);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   initWifi();
@@ -18,17 +17,14 @@ void setup()
   rqShibaAssets();
 }
 
-void loop()
-{
+void loop() {
   button.read();
 }
 
-void rqShibaAssets()
-{
+void rqShibaAssets() {
   const char *host = apiShiba;
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  if (WiFi.status() == WL_CONNECTED) {
     Serial.println("SIAMO CONNESSI RAGA!");
 
     WiFiClientSecure client;
@@ -40,57 +36,40 @@ void rqShibaAssets()
     http.begin(client, host);
 
     String payload;
-    if (http.GET() == HTTP_CODE_OK)
-    {
+    if (http.GET() == HTTP_CODE_OK) {
       payload = http.getString();
-      // Serial.println(payload);
       DynamicJsonDocument doc(1024);
 
       char *payload1 = (char *)malloc(http.getSize() + 1);
       http.getString().toCharArray(payload1, http.getSize() + 1);
-      Serial.println("PAYLOAD:");
-      Serial.println(payload1);
       http.end();
 
       DeserializationError error = deserializeJson(doc, payload1);
       free(payload1);
 
-      if (error)
-      {
+      if (error){
         Serial.print(F("deserializeJson() failed with code "));
         Serial.println(error.c_str());
       }
 
-      Serial.println("Eccoci:");
-      JsonObject obj = doc.as<JsonObject>();
-
-      if (obj.containsKey("data"))
-      {
-        Serial.println("Contiene data:");
-        for (JsonPair kv : obj)
-        {
-          Serial.println("KEY: ");
-          Serial.println(kv.key().c_str());
-
-          //            for(JsonVariant value: kv.key().c_str()){ // nope, WIP
-          //              Serial.println(value.as<char*>())
-          //            }
-
-          Serial.println("VALUE: ");
-          Serial.println(kv.value().as<char *>());
-        }
-      }
+      const char* symbol = doc["data"]["symbol"];
+      float price_usd = doc["data"]["market_data"]["price_usd"];
+      Serial.print("Symbol: ");
+      Serial.println(symbol);
+      Serial.println("-----------------");
+      Serial.print("Price: ");
+      Serial.println(price_usd, 6);
+      
       // serializeJsonPretty(doc, Serial);
     }
   }
+  
 }
 
-void initWifi()
-{
+void initWifi() {
   WiFi.begin(ssid, pwd);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
